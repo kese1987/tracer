10efc3778503e11f6da39ed312019836a76c532f
Enrico <kese87@gmail.com>
2018-03-09 22:22:59 +0100

    initial commit

---
+++ tracer/src/main/java/com/shareshipping/utils/tracer/App.java
@@ -0,0 +1,13 @@
+package com.shareshipping.utils.tracer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
